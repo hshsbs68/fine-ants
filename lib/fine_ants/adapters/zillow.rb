154c50a5101f7353be5017efbd7b80a3bdb84d4c
Zach Holman <zach@zachholman.com>
2020-04-21 17:31:20 -0700

    fix up zillow (maybe)

--- lib/fine_ants/adapters/zillow.rb
+++ lib/fine_ants/adapters/zillow.rb
@@ -12,28 +12,17 @@ module FineAnts
       end
 
       def download
-        visit "https://www.zillow.com/homedetails/total_nonsense/#{@user}_zpid/?fullpage=true"
-        zestimate = find_first(
-          ".estimates .home-summary-row:nth-child(2) span:nth-child(2)",
-          ".zestimate.primary-quote"
-        ).text.match(/(\$.*)/)[1]
+        visit "https://www.zillow.com/homedetails/xxxx/#{@user}_zpid/?fullpage=true"
+
+        zestimate = all('.ds-home-details-chip p span:last-child').last.text
 
         [{
           adapter: :zillow,
           user: @user,
           id: @user,
-          name: find_first(".addr h1", ".hdp-home-header-st-addr").text,
-          amount: BigDecimal(zestimate.gsub(/[\$,]/, "")),
+          name: find('.ds-address-container').text,
+          amount: zestimate.scan(/[.0-9]/).join.to_f
         }]
-      end
-
-      private
-
-      def find_first(*locators)
-        locators.each do |locator|
-          return find(locator)
-        rescue Capybara::ElementNotFound
-        end
       end
     end
   end
